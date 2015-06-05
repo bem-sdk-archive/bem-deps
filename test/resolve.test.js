caec8ef824455cabaf4b4472beab35d0a6b6345f
Ilia Isupov <matumbaman@yandex-team.ru>
2015-06-05 15:46:55 +0300

    Removed params from resove function mock in order to be able to run test (jshint does not allow unused params). Removed 'common' context from tests.

--- test/resolve.test.js
+++ test/resolve.test.js
@@ -2,8 +2,6 @@
  * Created by matumbaman on 05.06.15.
  */
 
-describe('common', function () {
-    describe('resolve', function () {
-        it('should proof tests working for now!');
-    });
+describe('resolve', function () {
+    it('should proof tests working for now!');
 });
