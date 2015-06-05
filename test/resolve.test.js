08dc64b57b658bf186696d471aeb15cf048e9f69
Ilia Isupov <matumbaman@yandex-team.ru>
2015-06-05 12:42:17 +0300

    Added test/resolve.test.js which will contain tests for resolve API. Contains 1 pending dummy for now.

---
+++ test/resolve.test.js
@@ -0,0 +1,9 @@
+/**
+ * Created by matumbaman on 05.06.15.
+ */
+
+describe('common', function () {
+    describe('resolve', function () {
+        it('should proof tests working for now!');
+    });
+});
