abd2f20a042e565ce6b83f5e9b058376819741b7
Ilia Isupov <matumbaman@yandex-team.ru>
2015-06-08 14:25:50 +0300

    Replaced returning null with returning {} in deps method stub in order to tests run correctly. Implemented basic tests for resolve method - covering input params checking behavior, blocks include order, exceptions on cyclic dependencies detection

--- test/resolve.test.js
+++ test/resolve.test.js
@@ -1,7 +1,176 @@
-/**
- * Created by matumbaman on 05.06.15.
- */
+var bemDeps = require('../lib/deps');
 
 describe('resolve', function () {
-    it('should proof tests working for now!');
+    it('should not process empty decl list', function () {
+        bemDeps.resolve()
+            .must.throw('The decl list is empty or not defined. It\'s impossible to to resolve empty decl list');
+    });
+
+    it('should return identical decl list if no deps specified', function () {
+        var decl = [
+            { block: 'A' },
+            { block: 'B' }
+        ];
+        bemDeps.resolve(decl).must.be.eql({
+            entities: decl,
+            dependOn: []
+        });
+    });
+
+    it('should include all blocks listed in decl even if they are missing in deps description', function () {
+        var decl = [
+                { block: 'A' },
+                { block: 'B' },
+                { block: 'C' },
+                { block: 'D' }
+            ],
+            deps = [
+                {
+                    entity: { block: 'C' },
+                    dependOn: {
+                        entity: { block: 'E' }
+                    }
+                }
+            ];
+        bemDeps.resolve(decl, deps).must.be.eql({
+            entities: [{ block: 'A' }, { block: 'B' }, { block: 'C' }, { block: 'D' }, { block: 'E' }],
+            dependOn: []
+        });
+    });
+
+    it('should not include dependency if it\'s missing in decl and nobody from decl references it', function () {
+        var decl = [
+                { block: 'A' },
+                { block: 'B' },
+                { block: 'C' }
+            ],
+            deps = [
+                {
+                    entity: { block: 'D' },
+                    dependOn: [
+                        {
+                            entity: { block: 'A' }
+                        }
+                    ]
+                }
+            ];
+        bemDeps.resolve(decl, deps).must.be.eql({
+            entities: [{ block: 'A' }, { block: 'B' }, { block: 'C' }],
+            dependOn: []
+        });
+    });
+
+    it('should place dependence before dependants', function () {
+        var decl = [
+                { block: 'A' },
+                { block: 'B' }
+            ],
+            deps = [
+                {
+                    entity: { block: 'A' },
+                    dependOn: [
+                        {
+                            entity: { block: 'B' },
+                            order: 'dependenceBeforeDependants'
+                        },
+                        {
+                            entity: { block: 'C' }
+                        }
+                    ]
+                }
+            ];
+        bemDeps.resolve(decl, deps).must.be.eql({
+            entities: [{ block: 'B' }, { block: 'A' }, { block: 'C' }],
+            dependOn: []
+        });
+    });
+
+    it('should keep the order of declaration', function () {
+        var decl = [
+           { block: 'A' },
+           { block: 'B' },
+           { block: 'C' },
+           { block: 'D' }
+
+        ],
+        deps = [
+            {
+                entity: { block: 'B' },
+                dependOn: {
+                    entity: { block: 'D' },
+                    order: 'dependenceBeforeDependants'
+                }
+            }
+        ];
+        bemDeps.resolve(decl, deps).must.be.eql({
+            entities: [{ block: 'A' }, { block: 'D' }, { block: 'B' }, { block: 'C' }],
+            dependOn: []
+        });
+    });
+
+    it('should not allow direct cyclic dependencies', function () {
+        var decl = [
+                { block: 'A' },
+                { block: 'B' }
+            ],
+            deps = [
+                {
+                    entity: { block: 'A' },
+                    dependOn: [
+                        {
+                            entity: { block: 'B' },
+                            order: 'dependenceBeforeDependants'
+                        }
+                    ]
+                },
+                {
+                    entity: { block: 'B' },
+                    dependOn: [
+                        {
+                            entity: { block: 'A' },
+                            order: 'dependenceBeforeDependants'
+                        }
+                    ]
+                }
+            ];
+        bemDeps.resolve(decl, deps).must.throw('Unable to process deps: detected cyclic reference A <- B <- A');
+    });
+
+    it('should not allow indirect cyclic depencencies', function () {
+        var decl = [
+                { block: 'A' },
+                { block: 'B' },
+                { block: 'C' }
+            ],
+            deps = [
+                {
+                    entity: { block: 'A' },
+                    dependOn: [
+                        {
+                            entity: { block: 'B' },
+                            order: 'dependenceBeforeDependants'
+                        }
+                    ]
+                },
+                {
+                    entity: { block: 'B' },
+                    dependOn: [
+                        {
+                            entity: { block: 'С' },
+                            order: 'dependenceBeforeDependants'
+                        }
+                    ]
+                },
+                {
+                    entity: { block: 'C' },
+                    dependOn: [
+                        {
+                            entity: { block: 'A' },
+                            order: 'dependenceBeforeDependants'
+                        }
+                    ]
+                }
+            ];
+        bemDeps.resolve(decl, deps).must.throw('Unable to process deps: detected cyclic reference A <- B <- C <- A');
+    });
 });
