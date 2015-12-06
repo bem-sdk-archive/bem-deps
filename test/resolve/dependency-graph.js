57afca41a2459a081a5f94864fc5349f4fd4a4c3
blond <andrewblond@yandex.com>
2015-12-06 03:15:22 +0300

    wip

--- test/resolve/dependency-graph.js
+++ test/resolve/dependency-graph.js
@@ -2,49 +2,113 @@ var expect = require('chai').expect,
     DependencyGraph = require('../../lib/resolve/dependency-graph');
 
 describe('resolve::DependencyGraph()', function () {
-    it('should return empty array if specified id is not exist', function () {
-        var dependencyGraph = new DependencyGraph();
+    describe('addDependency', function () {
+        it('should add dependency', function () {
+            var dependencyGraph = new DependencyGraph();
 
-        expect(dependencyGraph.getDependenciesById('non-existing')).to.be.deep.equal([]);
+            dependencyGraph.addDependency({ block: 'A' }, { block: 'B' });
+
+            expect(dependencyGraph.dependenciesOf({ block: 'A' })).to.be.deep.equal([
+                { block: 'B' }
+            ]);
+        });
+
+        it('should add same dependency only once', function () {
+            var dependencyGraph = new DependencyGraph();
+
+            dependencyGraph.addDependency({ block: 'A' }, { block: 'B' });
+            dependencyGraph.addDependency({ block: 'A' }, { block: 'B' });
+
+            expect(dependencyGraph.dependenciesOf({ block: 'A' })).to.be.deep.equal([
+                { block: 'B' }
+            ]);
+        });
+
+        it('should add another dependency to existing entity', function () {
+            var dependencyGraph = new DependencyGraph();
+
+            dependencyGraph.addDependency({ block: 'A' }, { block: 'B' });
+            dependencyGraph.addDependency({ block: 'A' }, { block: 'C' });
+
+            expect(dependencyGraph.dependenciesOf({ block: 'A' })).to.be.deep.equal([
+                { block: 'B' }, { block: 'C' }
+            ]);
+        });
     });
 
-    it('should return empty array if entity with specified id not has dependencies', function () {
-        var dependencyGraph = new DependencyGraph();
+    describe('dependenciesOf', function () {
+        it('should return empty array if specified id is not exist', function () {
+            var dependencyGraph = new DependencyGraph();
 
-        dependencyGraph.addDependency({ block: 'A' }, { block: 'B' });
+            expect(dependencyGraph.dependenciesOf({ block: 'non-existing' })).to.be.deep.equal([]);
+        });
 
-        expect(dependencyGraph.getDependenciesById('B')).to.be.deep.equal([]);
+        it('should return empty array if entity with specified id not has dependencies', function () {
+            var dependencyGraph = new DependencyGraph();
+
+            dependencyGraph.addDependency({ block: 'A' }, { block: 'B' });
+
+            expect(dependencyGraph.dependenciesOf({ block: 'B' })).to.be.deep.equal([]);
+        });
+
+        it('should return unordered dependencies', function () {
+            var dependencyGraph = new DependencyGraph();
+
+            dependencyGraph.addDependency({ block: 'A' }, { block: 'B' });
+
+            expect(dependencyGraph.dependenciesOf({ block: 'A' })).to.be.deep.equal([
+                { block: 'B' }
+            ]);
+        });
+
+        it('should return ordered dependencies', function () {
+            var dependencyGraph = new DependencyGraph();
+
+            dependencyGraph.addDependency({ block: 'A' }, { block: 'B' }, { order: 'dependenceBeforeDependants' });
+
+            expect(dependencyGraph.dependenciesOf({ block: 'A' })).to.be.deep.equal([
+                { block: 'B' }
+            ]);
+        });
     });
 
-    it('should add dependency', function () {
-        var dependencyGraph = new DependencyGraph();
+    describe('unorderedDependenciesOf', function () {
+        it('should not return ordered dependencies', function () {
+            var dependencyGraph = new DependencyGraph();
 
-        dependencyGraph.addDependency({ block: 'A' }, { block: 'B' });
+            dependencyGraph.addDependency({ block: 'A' }, { block: 'B' }, { order: 'dependenceBeforeDependants' });
 
-        expect(dependencyGraph.getDependenciesById('A')).to.be.deep.equal([
-            { block: 'B' }
-        ]);
+            expect(dependencyGraph.unorderedDependenciesOf({ block: 'A' })).to.be.deep.equal([]);
+        });
+
+        it('should return unordered dependencies', function () {
+            var dependencyGraph = new DependencyGraph();
+
+            dependencyGraph.addDependency({ block: 'A' }, { block: 'B' });
+
+            expect(dependencyGraph.unorderedDependenciesOf({ block: 'A' })).to.be.deep.equal([
+                { block: 'B' }
+            ]);
+        });
     });
 
-    it('should add same dependency only once', function () {
-        var dependencyGraph = new DependencyGraph();
+    describe('orderedDependenciesOf', function () {
+        it('should not return unordered dependencies', function () {
+            var dependencyGraph = new DependencyGraph();
 
-        dependencyGraph.addDependency({ block: 'A' }, { block: 'B' });
-        dependencyGraph.addDependency({ block: 'A' }, { block: 'B' });
+            dependencyGraph.addDependency({ block: 'A' }, { block: 'B' });
 
-        expect(dependencyGraph.getDependenciesById('A')).to.be.deep.equal([
-            { block: 'B' }
-        ]);
-    });
+            expect(dependencyGraph.orderedDependenciesOf({ block: 'A' })).to.be.deep.equal([]);
+        });
 
-    it('should add another dependency', function () {
-        var dependencyGraph = new DependencyGraph();
+        it('should return ordered dependencies', function () {
+            var dependencyGraph = new DependencyGraph();
 
-        dependencyGraph.addDependency({ block: 'A' }, { block: 'B' });
-        dependencyGraph.addDependency({ block: 'A' }, { block: 'C' });
+            dependencyGraph.addDependency({ block: 'A' }, { block: 'B' }, { order: 'dependenceBeforeDependants'});
 
-        expect(dependencyGraph.getDependenciesById('A')).to.be.deep.equal([
-            { block: 'B' }, { block: 'C' }
-        ]);
+            expect(dependencyGraph.orderedDependenciesOf({ block: 'A' })).to.be.deep.equal([
+                { block: 'B' }
+            ]);
+        });
     });
 });
