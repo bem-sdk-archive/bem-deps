fe57b386e48e538ab876076be7799633fb01d32b
blond <andrewblond@yandex.com>
2016-02-13 03:09:42 +0300

    wip

---
+++ test/utils/merge-iterators/series.js
@@ -0,0 +1,27 @@
+import iterators from '../../../lib/utils/iterators';
+import { expect } from 'chai';
+
+describe('`merge-iterators`.series()', function () {
+    it('should 1', function () {
+        expect(series()).to.be.empty;
+    });
+
+    it('should 2', function () {
+        expect(series([1, 2, 3])).to.be.deep.equal([1, 2, 3]);
+    });
+
+    it('should 3', function () {
+        expect(series([1, 2], [3, 4])).to.be.deep.equal([1, 2, 3, 4]);
+    });
+
+    it('should 4', function () {
+        expect(series([1], [], [3])).to.be.deep.equal([1, 3]);
+    });
+});
+
+function series(...arrs) {
+    var iters = arrs.map(arr => arr[Symbol.iterator]()),
+        iter = iterators.series(...iters);
+
+    return Array.from(iter);
+}
